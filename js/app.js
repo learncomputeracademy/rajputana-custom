d0b5227302c2e6c80d38a468991685e2495faa06
chiranjit2020 <chiranjit.rick002@gmail.com>
2020-12-15 12:34:49 +0530

    commit...

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 //Splide Slider
-new Splide('.splide', {
-    type: 'fade',
-    rewind: true,
-}).mount();
+//new Splide('.splide', {
+//    type: 'fade',
+//    rewind: true,
+//}).mount();
 
 //Modal
 const playBtn = document.querySelector(".play-btn");
@@ -26,3 +26,43 @@ function closeModal() {
     }
     Object.assign(modal.style, styles);
 }
+
+//Custom Build Section Slider
+window.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelector(".slider-items").children;
+    const prevSlide = document.querySelector(".left-slide");
+    const nextSlide = document.querySelector(".next-btn");
+    const totalSlide = slides.length;
+    let index = 0;
+
+
+    nextSlide.onclick = function () {
+        next("next");
+    }
+
+    prevSlide.onclick = function () {
+        next("prev");
+    }
+
+    function next(direction) {
+        if (direction == "next") {
+            index++;
+            if (index == totalSlide) {
+                index = 0;
+            }
+        } else {
+            if (index == 0) {
+                index = totalSlide - 1;
+                console.log(index)
+            } else {
+                index--;
+            }
+        }
+
+        //Add and Remove Active class 
+        for (let i = 0; i < totalSlide; i++) {
+            slides[i].classList.remove("active");
+        }
+        slides[index].classList.add("active");
+    }
+});
